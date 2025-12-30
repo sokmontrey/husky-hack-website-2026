96463535d9fdab4db48e7c067944ba3efb801fbc
Caio Cotts <caio@cotts.com.br>
2025-12-29 22:29:11 -0500

    test: Add more cases to form validation test

--- supabase/functions/submit-form/validateForm.test.ts
+++ supabase/functions/submit-form/validateForm.test.ts
@@ -3,28 +3,17 @@ import { assertEquals } from '@std/assert'
 import { formData, responseBody } from './model.ts'
 import validateFormData from './validateForm.ts'
 
+const nameHasSymbolsErrorMessage = 'Name contains invalid characters'
+const invalidEmailFormatErrorMessage = 'Email format is invalid'
+const missingEmailFieldErrorMessage = 'Email is required'
+
 describe('validateFormData', () => {
-  it('returns an empty body if the form is valid ', () => {
-    const form: formData = {
-      email: 'example@email.com.br',
-      firstName: 'joHn',
-      lastName: 'doE',
-    }
+  it('returns an empty body if the form is valid', () => {
+    const validEmails = ['example@email.com', 'hackathon+example@email.com']
 
-    const body = validateFormData(form)
-
-    const expected: responseBody = {
-      message: '',
-      error: {},
-      data: {},
-    }
-    assertEquals(body, expected)
-  })
-
-  describe('validation errors', () => {
-    it('adds an error to the email field for an invalid email entry', () => {
+    validEmails.forEach((email: string) => {
       const form: formData = {
-        email: 'exampleemail.com.br',
+        email: email,
         firstName: 'joHn',
         lastName: 'doE',
       }
@@ -33,34 +22,83 @@ describe('validateFormData', () => {
 
       const expected: responseBody = {
         message: '',
-        error: { email: ['Email format is invalid'] },
+        error: {},
         data: {},
       }
       assertEquals(body, expected)
     })
   })
 
-  it('adds an error to the name fields if they contain symbols', () => {
-    const form: formData = {
-      email: 'example@email.com.br',
-      firstName: 'jo Hn!',
-      lastName: '12doE',
-    }
+  describe('validation errors', () => {
+    it('adds an error to the email field for an invalid email entry', () => {
+      const invalidEmails = ['exampleemail.com.br', 'example@email']
 
-    const body = validateFormData(form)
+      invalidEmails.forEach((email: string) => {
+        const form: formData = {
+          email: email,
+          firstName: 'joHn',
+          lastName: 'doE',
+        }
 
-    const expected: responseBody = {
-      message: '',
-      error: {
-        firstName: ['Name contains invalid characters'],
-        lastName: ['Name contains invalid characters'],
-      },
-      data: {},
-    }
-    assertEquals(body, expected)
+        const body = validateFormData(form)
+
+        const expected: responseBody = {
+          message: '',
+          error: { email: [invalidEmailFormatErrorMessage] },
+          data: {},
+        }
+        assertEquals(body, expected)
+      })
+    })
   })
 
-  it('adds an error to each required field that is empty', () => {
+  it('adds an error to the name fields if they contain symbols', () => {
+    const namesWithSymbols = [
+      {
+        firstName: 'jo Hn!',
+        lastName: '12doE',
+        errors: {
+          firstName: [nameHasSymbolsErrorMessage],
+          lastName: [nameHasSymbolsErrorMessage],
+        },
+      },
+      {
+        firstName: 'john',
+        lastName: 'd0e',
+        errors: {
+          lastName: [nameHasSymbolsErrorMessage],
+        },
+      },
+      {
+        firstName: '+john',
+        lastName: 'doe',
+        errors: {
+          firstName: [nameHasSymbolsErrorMessage],
+        },
+      },
+    ]
+
+    namesWithSymbols.forEach(
+      ({ firstName, lastName, errors }) => {
+        const form: formData = {
+          email: 'example@email.com.br',
+          firstName: firstName,
+          lastName: lastName,
+        }
+
+        const body = validateFormData(form)
+
+        const expected: responseBody = {
+          message: '',
+          error: errors,
+          data: {},
+        }
+        assertEquals(body, expected)
+      },
+    )
+  })
+
+  it('adds an error for an empty email', () => {
     const form: formData = {
       email: '',
       firstName: '',
@@ -72,7 +110,7 @@ describe('validateFormData', () => {
     const expected: responseBody = {
       message: '',
       error: {
-        email: ['Email is required'],
+        email: [missingEmailFieldErrorMessage],
       },
       data: {},
     }
