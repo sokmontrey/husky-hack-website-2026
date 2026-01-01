e2f9ec3f281f6db3a9cf1b454f88a217dae0c8c7
Sokmontrey <sythatsokmontrey@gmail.com>
2026-01-01 18:21:14 -0500

    feat: Add new image and scribble components, and update About Us section content and layout.

--- src/components/landing-page-sections/AboutUsSection.tsx
+++ src/components/landing-page-sections/AboutUsSection.tsx
@@ -1,14 +1,43 @@
+import Image from "next/image";
+import huskyUseComputer from "../../assets/husky-use-computer.png";
+import Underline1 from "../scribbles/Underline1";
+import Star1 from "../scribbles/Star1";
+
 export default function AboutUsSection() {
     return (
-        <div id='About-Us' className='py-10 border-t-2 border-b-2 border-gray-200'>
-            <div className="">
-                <h1 className='text-3xl text-center'>About Us</h1>
-                <p className="text-sm text-gray-600">
-                    {/* A hackathon run with &lt;3 by programmers, designers, innovators, and most important of all, storytellers.
-                    A student-run hackathon for innovators, artists, and storytellers. */}
+        <div id="About-Us" className="py-10 flex flex-row gap-2">
+            <div className="flex flex-row gap-2">
+                <Image
+                    src={huskyUseComputer}
+                    alt="Husky Use Computer"
+                    className="h-3/4 mt-5"
+                    width={400}
+                    height={400}
+                />
 
-                    {" A student-run hackathon by a group of friends who think they can make it happen and now it's too late to turn back."}
-                </p>
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-row">
+                        <div className="w-fit">
+                            <h1 className="text-4xl font-serif">
+                                About HuskyHack
+                            </h1>
+                            <Underline1 className="text-neutral-700 pl-4 py-2 pr-4" />
+                        </div>
+                        <Star1 className="text-sky-400 w-8 h-8 ml-4 -translate-y-5" />
+                        <Star1 className="text-amber-500 w-5 h-6 -translate-y-1 rotate-3 -scale-x-100" />
+                    </div>
+
+                    <p className="text-sm text-gray-600 font-sans mt-2 mb-4">
+                        Everyone has a story to tell. Turn yours into something
+                        real alongside 100+ new friends in the heart of Toronto.
+                    </p>
+                    <p className="text-sm text-gray-600 font-sans">
+                        Join us at HuskyHack on May 1st-2nd for a 24-hour
+                        hackathon to build solutions that bring ideas to life.
+                        We'll handle the space, food, and coffee; you just need
+                        to bring a team, or join one when you get here.
+                    </p>
+                </div>
             </div>
         </div>
     );
