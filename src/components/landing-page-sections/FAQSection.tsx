cd6a489e95d2365af1a947be4bff4432a7126392
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2026-01-26 20:47:56 -0500

    Fixed key prop warning message

--- src/components/landing-page-sections/FAQSection.tsx
+++ src/components/landing-page-sections/FAQSection.tsx
@@ -9,15 +9,15 @@ export default function FAQSection() {
             <h1 className="text-4xl font-serif text-center mb-10">FAQs</h1>
 
             <div className="flex flex-col justify-center items-center md:flex-row-reverse gap-4 w-full ">
-           
+
                 <div className="flex flex-col gap-4 w-[50vw] items-start">
                     {faqs.map((faq, index) => (
 
-                        <div className="bg-[#172334] rounded-lg w-[50vw] text-white">
-                           <Accordion
-                            key={index}
-                            question={faq.question}
-                            answer={faq.answer}                        />
+                        <div className="bg-[#172334] rounded-lg w-[50vw] text-white" key={index}>
+
+                            <Accordion
+                                question={faq.question}
+                                answer={faq.answer} />
                         </div>
                     ))}
                 </div>
