df225ee25dcd0faed5ff750e4a86571fbdcba0b8
Sokmontrey <sythatsokmontrey@gmail.com>
2026-02-03 20:01:05 -0500

    fix: update hero header typo and adjust the about us

--- src/components/landing-page-sections/AboutUsSection.tsx
+++ src/components/landing-page-sections/AboutUsSection.tsx
@@ -3,7 +3,10 @@ import danielCampus from "../../assets/gbp-daniels-campus.png";
 import studentsLearning from "../../assets/gbp-students-learning.png";
 export default function AboutUsSection() {
     return (
-        <section className="relative w-full min-h-[75vh] overflow-hidden" id="About-Us">
+        <section
+            className="relative w-full min-h-[75vh] overflow-hidden"
+            id="About-Us"
+        >
             <div className="absolute flex flex-row items-center bottom-0 z-20 h-full w-full justify-between px-[20px]">
                 <div className="hidden absolute bottom-0 w-1/4 relative h-auto top-[100px] md:block">
                     <Image
@@ -15,17 +18,23 @@ export default function AboutUsSection() {
                     />
                 </div>
                 <div className="flex-1 w-full relative flex flex-col items-center top-0 h-full gap-4 md:w-3/4">
-                    <h2 className="text-white text-3xl font-rethink font-semibold">ABOUT US</h2>
-                    <h1 className="text-4xl md:text-6xl text-[#FED571] text-center font-rethink font-bold">
-                        GBP largest student-run hackathon
-
+                    <h2 className="text-white text-3xl font-rethink font-semibold">
+                        ABOUT US
+                    </h2>
+                    <h1 className="text-4xl md:text-6xl text-[#FED571] text-center font-rethink font-base">
+                        George Brown Polytechnic's{" "}
+                        <strong>largest student-run</strong> hackathon
                     </h1>
-                    <p className="italic text-lg text-white text-center font-instrument md:not-italic md:text-xl">Spend 24h building something you're passionate about with food and coffee taking care of at the heart of Toronto.
+                    <p className="italic text-lg text-gray-300 text-center font-instrument md:not-italic md:text-xl">
+                        Spend <strong className="text-white">24 hours</strong>{" "}
+                        in the heart of Toronto building something you're
+                        passionate about, food and caffeine is on us.{" "}
+                        <strong className="text-white">Find your pack</strong>{" "}
+                        among 150+ other to form an unstoppable team and
+                        unforgettable memories
                     </p>
-                    <p className="italic text-lg text-white text-center font-instrument md:not-italic md:text-xl"> But our hackathon is more than just code—it’s a place to learn, collaborate, and
-                        find your people among 100+ other to form an unstoppable team and unforgettable memories</p>
                 </div>
-                <div className="absolute bottom-0 hidden w-1/4 relative h-auto top-[100px] md:block">
+                <div className="relative bottom-0 hidden w-1/4 h-auto top-[100px] md:block">
                     <Image
                         src={danielCampus}
                         alt="Daniels Campus"
@@ -46,7 +55,5 @@ export default function AboutUsSection() {
                 />
             </div>
         </section>
-
-
     );
 }
