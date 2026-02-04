1cf791158fc14845b5f7ea9bed73d59d51b75aeb
Sokmontrey <sythatsokmontrey@gmail.com>
2026-02-04 17:33:18 -0500

    fix: cave background responsiveness

--- src/utils/landingPage/CaveDecorationWrapper.tsx
+++ src/utils/landingPage/CaveDecorationWrapper.tsx
@@ -2,31 +2,31 @@ import Image from "next/image";
 
 const CaveDecorationWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div  className="relative pt-12 md:pt-48 text-white overflow-hidden bg-[linear-gradient(to_bottom,#1C6D41,#243B5C,#090F18,#030609)] min-h-screen h-auto">
-                  {/* Background  */}
-       <div className="md:opacity-0 lg:opacity-100">
-        <div className="absolute top-0 left-0 h-full w-auto z-0 pointer-events-none lg:opacity-100">
-                <Image
-                    src={"/expectations/left_cave.svg"}
-                    alt="Cave Left"
-                    className="h-full w-auto object-cover md:max-w-none translate-x-[-50%] md:translate-x-0"
-                    width={500}
-                    height={1000}
-                    unoptimized
-                />
-            </div>
+        <div className="relative pt-12 md:pt-48 text-white overflow-hidden bg-[linear-gradient(to_bottom,#1C6D41,#243B5C,#090F18,#030609)] min-h-screen h-auto">
+            {/* Background  */}
+            <div className="opacity-0 md:opacity-100 lg:opacity-100">
+                <div className="absolute top-0 left-0 h-full w-auto z-0 pointer-events-none lg:opacity-100">
+                    <Image
+                        src={"/expectations/left_cave.svg"}
+                        alt="Cave Left"
+                        className="h-full w-auto object-cover md:max-w-none translate-x-[-50%] md:translate-x-0"
+                        width={500}
+                        height={1000}
+                        unoptimized
+                    />
+                </div>
 
-            <div className="absolute top-0 right-0 h-full w-auto z-0 pointer-events-none lg:opacity-100">
-                <Image
-                    src={"/expectations/right_cave.svg"}
-                    alt="Cave Right"
-                    className="h-full w-auto object-cover md:max-w-none translate-x-[50%] md:translate-x-0"
-                    width={500}
-                    height={1000}
-                    unoptimized
-                />
+                <div className="absolute top-0 right-0 h-full w-auto z-0 pointer-events-none lg:opacity-100">
+                    <Image
+                        src={"/expectations/right_cave.svg"}
+                        alt="Cave Right"
+                        className="h-full w-auto object-cover md:max-w-none translate-x-[50%] md:translate-x-0"
+                        width={500}
+                        height={1000}
+                        unoptimized
+                    />
+                </div>
             </div>
-        </div> 
 
             {children}
         </div>
